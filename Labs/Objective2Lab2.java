08e757e39536b933401e1bc5893868b9ff8e4207
Daniel Majewski <d.majewski.3453@gmail.com>
2020-07-30 23:40:02 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Daniel Majewski";
+    age = 31;
+    state = "Virginia";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
