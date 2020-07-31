50f1095a9ad2009d94a8c69278afade3bdc04009
Daniel Majewski <d.majewski.3453@gmail.com>
2020-07-31 02:07:31 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 55.0;
+    num2 = 150.0;
+    num3 = 17.0;
+    average = (num1 + num2 + num3) / 3.0;
+    System.out.println(average);
+  }
+}
