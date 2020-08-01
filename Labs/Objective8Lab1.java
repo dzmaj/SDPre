dbd3359900eddbc07a4082b2c17f7a847186e9c4
Daniel Majewski <d.majewski.3453@gmail.com>
2020-07-31 23:17:05 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i;
+    for(i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+    for(i = 10; i > 0; i--) {
+      System.out.println(i);
+    }
+  }
+}
